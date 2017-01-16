58c1bb304c8f0b114632a62922dafaf4c221a6c4
Pablo Tesone <tesonep@gmail.com>
2017-01-16 21:54:02 +0100

    Fixing travis process

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -3,6 +3,10 @@
 set -ex
 
 ./pharo Pharo.image eval "
+((MCSmalltalkhubRepository
+owner: 'Pharo'
+project: 'Pharo60Inbox') versionFrom: 'SLICE-Issue-19547-Extracting-the-creation-of-refactoring-changes-to-a-factory-PabloTesone.1') load.
+
 Metacello new 
 	baseline: 'AtomicRefactors';
 	repository: 'filetree://.';
