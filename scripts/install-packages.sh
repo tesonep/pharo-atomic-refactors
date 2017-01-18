03e71f3938f62ce626a8a58172fa798397610036
Pablo Tesone <tesonep@gmail.com>
2017-01-18 14:19:50 +0100

    Testing the slice, and the refactorings2 separately

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -2,11 +2,24 @@
 
 set -ex
 
-./pharo Pharo.image eval "
+./pharo Pharo.image eval --save "
 ((MCSmalltalkhubRepository
 owner: 'Pharo'
 project: 'Pharo60Inbox') versionFrom: 'SLICE-Issue-19547-Extracting-the-creation-of-refactoring-changes-to-a-factory-PabloTesone.1') load.
+"
 
+./pharo Pharo.image eval "
+Gofer new
+url: 'http://smalltalkhub.com/mc/GustavoSantos/Refactoring2/main';
+package: 'ConfigurationOfRefactoring2';
+load.
+
+(Smalltalk at: #ConfigurationOfRefactoring2) loadDevelopment.
+
+Smalltalk saveAs:'refactoring2'.
+"
+
+./pharo Pharo.image eval "
 Metacello new 
 	baseline: 'AtomicRefactors';
 	repository: 'filetree://.';
